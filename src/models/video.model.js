a60b2830c1dd2063d6dc8974e95e8e0c8d65485a
snsumit <ss2153436@gmail.com>
2024-12-27 22:34:28 +0530

    added files

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,7 @@
 import mongoose,{Schema} from "mongoose"
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 const videoSchema = new Schema({
+    
     videoFile:{
         type:String,
         required:true,
